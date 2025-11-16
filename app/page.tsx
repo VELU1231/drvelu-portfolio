7d05d3a431b6689f158559c8c15011d3ce0e8490
Velu Murugan <thalaveldaa1201@gmail.com>
2025-11-16 09:39:50 +0800

    Fix: Change Hero import to default import (matches Hero.tsx export)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { Projects } from './components/Projects';
+import Hero from './components/Hero';import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
