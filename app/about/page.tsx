6e0b764123242280cf3318652c34c186650adfd7
Velu Murugan <thalaveldaa1201@gmail.com>
2025-11-16 09:34:55 +0800

    Fix: Correct about page imports - use relative paths with proper formatting

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,7 @@
-import { Navbar } from '../components/Navbar';import { Footer } from '@/app/components/Footer';
+'use client';
+
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
 
 export default function About() {
   return (
